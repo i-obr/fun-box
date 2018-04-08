de7224ddec1b93ad4c09dafc0d7a3461fe1794fb
i-obr <obr.iv@ya.ru>
2018-04-08 14:35:33 +0300

    add stylelint

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -3,6 +3,7 @@ import precss from 'precss';
 import plumber from 'gulp-plumber';
 import postcss from 'gulp-postcss';
 import flexbugs from 'postcss-flexbugs-fixes';
+import normalize from 'postcss-normalize';
 import autoprefixer from 'autoprefixer';
 import mqpacker from 'css-mqpacker';
 import csso from 'gulp-csso';
@@ -24,6 +25,7 @@ function style() {
         precss,
         flexbugs,
         autoprefixer,
+        normalize,
         mqpacker({
           sort: true,
         }),
