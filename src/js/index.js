5383f2451549c17bde98f742a4f065ff1f59f543
i-obr <obr.iv@ya.ru>
2018-04-11 09:16:35 +0300

    fix(index.html, index.js)

--- src/js/index.js
+++ src/js/index.js
@@ -6,26 +6,28 @@ products.forEach((product) => {
   const card = product.querySelector('.product__card');
   const heading = product.querySelector('.product__heading');
 
-  if (checkbox.hasAttribute('disabled')) {
-    product.classList.add('is-disabled');
-    buy.innerHTML = product.dataset.disabled;
-  }
-
-  checkbox.addEventListener('change', () => {
+  const handlerSatusProduct = () => {
     if (!checkbox.hasAttribute('checked')) {
       checkbox.setAttribute('checked', true);
       product.classList.add('is-selected');
       buy.classList.add('visually-hidden');
       heading.classList.remove('visually-hidden');
     } else {
-      product.classList.remove('is-selected');
       checkbox.removeAttribute('checked');
+      product.classList.remove('is-selected');
       buy.classList.remove('visually-hidden');
       heading.classList.add('visually-hidden');
       product.classList.remove('is-selected-hover');
     }
-  });
+  };
 
+  if (checkbox.hasAttribute('disabled')) {
+    product.classList.add('is-disabled');
+    buy.innerHTML = product.dataset.disabled;
+  }
+
+  checkbox.addEventListener('change', handlerSatusProduct);
+  card.addEventListener('click', handlerSatusProduct);
   card.addEventListener('mouseleave', () => {
     if (checkbox.hasAttribute('checked')) {
       product.classList.add('is-selected-hover');
